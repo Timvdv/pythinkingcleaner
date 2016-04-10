1057a8d9768aa77671576d7600808486f924d8d9
Dennis Karpienski <dennis@karpienski.de>
2016-04-10 14:05:42 +0200

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pythinkingcleaner',
-      version='0.0.1',
+      version='0.0.2',
       description='Library to interface with thinkingcleaner',
       url='https://github.com/TheRealLink/pythinkingcleaner',
       author='Dennis Karpienski',
